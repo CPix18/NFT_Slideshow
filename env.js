2c4431e59f304112c11fcd6b6b4700bf68008e1d
Collin Pixley <39272924+CPix18@users.noreply.github.com>
2023-01-17 10:18:32 -0600

    update env.js

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
    env = {
-     SHEET_BEST_CONNECTION_URL: 'your-connection-url',
+     SHEET_BEST_CONNECTION_URL: 'https://docs.google.com/spreadsheets/d/1mk0K1MXuIx3RwVkzpTk7yxD2ykSFgsUYKQReIDbYz6M/edit?usp=sharing',
      SLIDE_TIMING: 60
    }
